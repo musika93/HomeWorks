e379cefb96da0a46629bf0f13a4256b78cdfa86d
Acer <Acer@GSCOMP>
2020-10-24 01:27:48 +0400

    задание 6

---
+++ HomeWorks/Lesson_2/RandomGeneratorDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks.Lesson_2
+{
+    class RandomGeneratorDemo
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+
+            int firstRandomNumber = randomGenerator.Next();
+            int secondRandomNumber = randomGenerator.Next();
+            int thirdRandomGenerator = randomGenerator.Next();
+            Console.WriteLine($"The first = {firstRandomNumber}, The second = {secondRandomNumber}, The third = {thirdRandomGenerator}");
+            Console.WriteLine($"Amount = {firstRandomNumber + secondRandomNumber + thirdRandomGenerator}");
+            Console.ReadKey();
+        }
+    }
+}
