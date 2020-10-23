3e68a5e098b6791cec8a38dc5bfa9870d8c924b2
Acer <Acer@GSCOMP>
2020-10-23 18:28:58 +0400

    Добавила второй урок без первого задания.

---
+++ HomeWorks/Lesson_2/Level_1-Task_5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks.Lesson_2
+{
+    class Level_1_Task_5
+    {
+        static void Main(string[] args)
+        {
+            /*Продемонстрировать работу с операциями сложения, вычитания, умножения и деления, и вывести результат вычислений на экран. */
+
+            // Дано 558 и 125, найти сумму (amount).
+            
+            // Дано 90 и 10, найти разность (difference).
+            
+            // Дано 200 и 5, найти частное (quotient).
+            
+            // Дано 59 и 11, найти произведение (productOfNumber).
+            
+
+            Console.ReadKey();
+        }
+    }
+}
